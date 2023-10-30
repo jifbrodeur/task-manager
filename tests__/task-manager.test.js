fa68e9d30b16e69ff79fd8dea8a2f66102de66ab
Jean-François Brodeur <jifbrodeur@jbmlogic.com>
2023-10-30 08:36:53 -0400

    ajouter tests unitaires avec Jest

---
+++ tests__/task-manager.test.js
@@ -0,0 +1,21 @@
+// __tests__/task-manager.test.js
+
+const taskManager = require("../task-manager");
+
+test("ajouterTache devrait ajouter une tache a la liste", () => {
+  taskManager.ajouterTache("Faire épicerie");
+  expect(taskManager.listerTaches()).toEqual([
+    { tache: "Faire épicerie", complete: false },
+  ]);
+});
+
+test("completerTache devrait changer l'état à compléte", () => {
+  taskManager.completerTache(0);
+  expect(taskManager.listerTaches()[0].complete).toBe(true);
+});
+
+test("listerTaches devrait retourner la liste des taches", () => {
+  expect(taskManager.listerTaches()).toEqual([
+    { task: "Faire épicerie", complete: true },
+  ]);
+});
