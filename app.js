0bc89199422f135bffe23fd65b37bddee4ea164e
Jean-François Brodeur <jifbrodeur@jbmlogic.com>
2023-10-30 08:20:54 -0400

    completer bugfix

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // app.js
 
-const taskManager = require("./task-manager");
+const taskManager = require("./task-manager.js");
 
-taskManager.addTask("Faire épicerie");
-taskManager.addTask("Finir devoir");
-taskManager.addTask("Aller au collège");
-taskManager.completeTask(0);
-taskManager.completeTask(1);
+taskManager.ajouterTache("Faire épicerie");
+taskManager.ajouterTache("Finir devoir");
+taskManager.ajouterTache("Aller au collège");
+taskManager.completerTask(0);
+taskManager.completerTask(1);
 taskManager.listerTaches().forEach((tache, index) => {
   console.log(
     `Task ${index + 1}: ${tache.tache} (Completed: ${tache.complete})`
