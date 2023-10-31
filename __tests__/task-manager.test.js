2842385ece29dfc4488cf84dc78dbc070ada0928
Jean-François Brodeur <jifbrodeur@jbmlogic.com>
2023-10-30 20:55:21 -0400

    corriger package.json et test

--- __tests__/task-manager.test.js
+++ __tests__/task-manager.test.js
@@ -9,13 +9,13 @@ test("ajouterTache devrait ajouter une tache a la liste", () => {
   ]);
 });
 
-test("completerTache devrait changer l'état à compléte", () => {
+test("completerTache devrait changer l'état à complété", () => {
   taskManager.completerTache(0);
   expect(taskManager.listerTaches()[0].complete).toBe(true);
 });
 
 test("listerTaches devrait retourner la liste des taches", () => {
   expect(taskManager.listerTaches()).toEqual([
-    { task: "Faire épicerie", complete: true },
+    { tache: "Faire épicerie", complete: true },
   ]);
 });
